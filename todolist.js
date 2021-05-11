e15b92260ace66acdcca6bef9fa3931367556b17
davidtchiu <dchiu@pugetsound.edu>
2021-05-11 14:24:19 -0700

    Initial commit

---
+++ todolist.js
@@ -0,0 +1,34 @@
+let todolist = [];
+let choice = "";
+while (choice != "quit") {
+  choice = prompt("Your choice?");
+  choice = choice.toLowerCase();
+  switch (choice) {
+    case "list":
+      listAction();
+      break;
+    case "add":
+      let newItem = prompt(`Enter a todo item`);
+      todolist.push(newItem);
+      break;
+    case "remove":
+      let itemNumToRemove = parseInt(prompt("ID of item to remove?"));
+      if (itemNumToRemove < todolist.length) {
+        todolist.splice(itemNumToRemove, 1);
+      } else {
+        console.log(`Item #${itemNumToRemove} doesn't exist!`);
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+/**
+ * What to do if 'list' is selected
+ */
+function listAction() {
+  for (let i = 0; i < todolist.length; i++) {
+    console.log(i, todolist[i]);
+  }
+}
